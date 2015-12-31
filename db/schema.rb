b70ff572c20174a3ac963d2b023d01f797362e34
Sky Su <februaryskys@gmail.com>
2015-12-31 17:52:45 +0000

    Generate a HABTM association between recipe and ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231162141) do
+ActiveRecord::Schema.define(version: 20151231170508) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20151231162141) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "ingredients_recipes", id: false, force: :cascade do |t|
+    t.integer "recipe_id",     null: false
+    t.integer "ingredient_id", null: false
+  end
+
+  add_index "ingredients_recipes", ["ingredient_id", "recipe_id"], name: "index_ingredients_recipes_on_ingredient_id_and_recipe_id"
+  add_index "ingredients_recipes", ["recipe_id", "ingredient_id"], name: "index_ingredients_recipes_on_recipe_id_and_ingredient_id"
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
