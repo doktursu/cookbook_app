aaa6bd115eb859db1cf0d905dfb734a60be3939d
Sky Su <februaryskys@gmail.com>
2015-12-31 16:50:11 +0000

    Adds ingredients edit and destroy

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -12,6 +12,20 @@ class IngredientsController < ApplicationController
     redirect_to ingredients_path
   end
 
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    Ingredient.find(params[:id]).update(ingredient_params)
+    redirect_to ingredients_path
+  end
+
+  def destroy
+    Ingredient.find(params[:id]).destroy
+    redirect_to ingredients_path
+  end
+
   private
     def ingredient_params
       params.require(:ingredient).permit(:name, :image)
